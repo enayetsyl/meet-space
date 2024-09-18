7b118e7f1f698f537c25e5e6ad53cf6a06de4b10
enayetaitech <mdrehman@365aitech.com>
2024-09-18 22:15:24 +0600

    live link changed

--- src/components/pages/Payment/CheckoutForm.tsx
+++ src/components/pages/Payment/CheckoutForm.tsx
@@ -21,7 +21,7 @@ const CheckoutForm = ({ room, slot, user }:{room:Room, slot:Slot, user:User}) =>
 
     
 
-    const returnUrl = `http://localhost:5173/payment-success?roomId=${room._id}&slotId=${slot._id}&userId=${user._id}`;
+    const returnUrl = `https://meet-space-theta.vercel.app/payment-success?roomId=${room._id}&slotId=${slot._id}&userId=${user._id}`;
 
     const { error } = await stripe.confirmPayment({
       elements,
